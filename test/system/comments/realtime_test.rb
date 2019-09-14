064062cd0624f9a772d820b1bc32a53c2250e4ce
Mike Rogers <me@mikerogers.io>
2019-09-14 19:56:59 +0100

    Adding outer spec

---
+++ test/system/comments/realtime_test.rb
@@ -0,0 +1,22 @@
+require "application_system_test_case"
+
+class Comments < ActiveSupport::TestCase
+  class RealtimeTest < ApplicationSystemTestCase
+    test "New comment updates index with latest content" do
+      visit comments_url
+
+      new_comment = Comment.new(title: 'Realtime title', body: 'Realtime Body')
+
+      assert_no_text new_comment.title
+
+      # Save & fire off the ActionCable job
+
+      # Wait for the new contents to be added to the page
+      Timeout.timeout(Capybara.default_max_wait_time) do
+        loop until page.has_content?(new_comment.title)
+      end
+
+      assert_text new_comment.title
+    end
+  end
+end
