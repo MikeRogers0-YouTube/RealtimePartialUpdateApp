d0988c1d559223b785ad515f4e0921bb3d6e7efe
Mike Rogers <me@mikerogers.io>
2019-09-16 13:52:12 +0100

    A quick note about why I used to_gid_param

--- app/jobs/partials/single_object_job.rb
+++ app/jobs/partials/single_object_job.rb
@@ -2,6 +2,8 @@ class Partials::SingleObjectJob < ApplicationJob
   queue_as :default
 
   def perform(object)
+    # .to_gid_param returns a unique reference to the object, so we can uniquely
+    # reference it in our views without revealing the ID.
     PartialsChannel.broadcast_to(object.to_gid_param, {
       body: ApplicationController.render(object, layout: false)
     })
