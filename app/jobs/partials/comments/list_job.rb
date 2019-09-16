8d1f133735a13fdb059102eb846c97c129a5d1af
Mike Rogers <me@mikerogers.io>
2019-09-16 14:59:35 +0100

    Commenting what the action does

--- app/jobs/partials/comments/list_job.rb
+++ app/jobs/partials/comments/list_job.rb
@@ -1,6 +1,7 @@
 class Partials::Comments::ListJob < ApplicationJob
   queue_as :default
 
+  # This will push the updated partial to the user via ActionCable.
   def perform
     RealtimePartialChannel.broadcast_to('comments/list', {
       body: ApplicationController.render(Comment.all, layout: false)
